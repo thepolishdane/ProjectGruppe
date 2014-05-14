0b1adc98b193cbd15da43594f6b09406298a4668
Kvetter <kasper.vetter@mail.dk>
2014-05-14 13:29:07 +0200

    Sql statement updateret og virker "næsten"
    Har ændret sql statementet så at det virker, når det kører som en
    template, vær opmærksom at table-navne og database navn er dem som jeg
    bruger på min computer, så de skal muligvis ændres hvis i prøver på
    jeres

--- testtemplate.php
+++ testtemplate.php
@@ -17,13 +17,12 @@ global $wpdb;
                 <td style="font-weight:bold">
                     Tilmeld Liga
                 </td>
-
             </tr>
             <tr>
                 <td>
                     <?php 
-                        $my_ligaer = $wpdb->get_row("SELECT * FROM wp_liga");
-                        echo $my_ligaer->liga_name ." - ". $my_ligaer->liga_lenght ." - ". $my_ligaer->liga_game;
+                        $my_ligaer = $wpdb->get_row("SELECT * FROM wp_ligaer");
+                        echo $my_ligaer->liga_navn ." - ". $my_ligaer->liga_længde ." - ". $my_ligaer->liga_spil;
                     ?>
                 </td>
             </tr>
@@ -42,10 +41,9 @@ global $wpdb;
                 </td>
             </tr>
         </table>
-        <form method="">
+        <form method="POST">
         <table border=1 style="float:left; width:20%; margin-left:1%; margin-top:2%">
             <tr>
-
                 <td style="font-weight:bold">
                     Opret Liga
                 </td>
@@ -79,29 +77,29 @@ global $wpdb;
                     <input type="reset" value="Reset">
                 </td>
                 <td>
-                    <button type="button" name="opret">Opret Liga</button>
+                    <input type ="submit" value ="Opret Liga" name="submit">
                 </td>
             </tr>
         </table>
-        <?php
-        if(isset ($_POST['opret'])){
-            $liga_navn = $_POST['liganavn'];
-            $liga_lenght = $_POST['ligalenght'];
-            $game = $_POST['game'];
-
-            $wpdb->insert('WP_liga', array( 
-        'column1' => $liga_navn, 
-        'column2' => $liga_lenght,
-        'column3' => $game 
-    ));
-
-        }
-
-
-        ?>
     </form>
-    </body>
 </html>
+<?php
+$name = $_POST[liganavn];
+$length = $_POST[ligalenght];
+$game = $_POST[game];
+
+
+if (isset($_POST['submit'])) {
+
+    try {
+        $wpdb->insert('WP_LIGAER', array('liga_navn' => $name, 'liga_længde' => $lenght, 'liga_spil' => $game));
+         echo "Følgende liga er blevet oprettet: $name $length $game";
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        $conn->rollback();
+    }
+}
+?>
 <?php
 get_sidebar();
 get_footer();
